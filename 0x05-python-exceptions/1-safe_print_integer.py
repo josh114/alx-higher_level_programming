e3f0c7e2c09918c9e3665ef6a0aab74e58cc49ab
josh114 <freejosh02@gmail.com>
2022-07-18 18:28:46 +0100

    correct output

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         if value/value == 1:
-            print('{:d}'.format(value))
+            print('{:d}'.format(value), end='\n')
             return True
     except TypeError:
         return False

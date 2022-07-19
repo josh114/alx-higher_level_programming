0b940e3eadebac1d545f0e37d9bfbaff70077594
josh114 <freejosh02@gmail.com>
2022-07-19 22:14:35 +0100

    correct output

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ import sys
 def safe_function(fct, *args):
     try:
         res = fct(*args)
-        return (res)
+        return res
     except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        print("Exception: {}".format(sys.exc_info()[1]), file = sys.stderr)
+        return None

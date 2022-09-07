9230d035e94ee2269b5135b6c8327f81949bfc6f
josh114 <freejosh02@gmail.com>
2022-09-07 16:35:10 +0100

    initial complete upload

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- id not empty 
+CREATE TABLE IF NOT EXIST id_not_null (id INT DEFAULT 1, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- unique id
+CREATE TABLE IF NOT EXIST unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));

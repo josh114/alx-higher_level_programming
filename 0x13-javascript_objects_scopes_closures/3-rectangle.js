8291a6673c7609babe0e8319ee99f310ea7db88d
josh114 <freejosh02@gmail.com>
2022-10-05 17:07:15 +0100

    correct task semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
-    let stringToDisplay = '';
+
+  print () {
     for (let i = 1; i <= this.height; i++) {
-      for (let j = 1; j <= this.width; j++) {
-        stringToDisplay += 'X';
-      }
-      stringToDisplay += '\n';
+      console.log('X'.repeat(this.width));
     }
-    console.log(stringToDisplay);
   }
 };

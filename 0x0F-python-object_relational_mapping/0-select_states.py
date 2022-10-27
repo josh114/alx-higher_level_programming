669266f4b4b6811212e0a6dadca4cae3b6f995e1
josh114 <freejosh02@gmail.com>
2022-10-27 21:22:50 +0100

    correct output

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# script that lists all states from the database hbtn_0e_0_usa
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
 import MySQLdb
-'''module validates username, passwd and db'''
-if __name__ == '__main__':
-    '''setup db connect and validation'''
+
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    '''execute query'''
-    c.execute('SELECT * FROM `states`')
+    c.execute("SELECT * FROM `states`")
     [print(state) for state in c.fetchall()]

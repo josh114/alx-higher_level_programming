583dab4d2156ce024dbb019382a57fe7a0b640cf
josh114 <freejosh02@gmail.com>
2022-10-27 21:20:46 +0100

    module documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,8 +2,11 @@
 # script that lists all states from the database hbtn_0e_0_usa
 import sys
 import MySQLdb
+'''module validates username, passwd and db'''
 if __name__ == '__main__':
+    '''setup db connect and validation'''
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
+    '''execute query'''
     c.execute('SELECT * FROM `states`')
     [print(state) for state in c.fetchall()]
